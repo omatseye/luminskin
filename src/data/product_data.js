51a5dcf2a5ec36e7f3ccdfe1f82ea59959dc71bc
peter <omatseye.clarke@gmail.com>
2021-02-11 21:19:50 +0100

    Create a React app and publish it to GitHub Pages

---
+++ src/data/product_data.js
@@ -0,0 +1,75 @@
+const product_card = [
+    {
+        id: 1,
+        product_name: "Age Management Set",
+        description: "Stay primetime, all the time.",
+        price: 350,
+        currency: "$",
+        thumb: "./images/1.png"
+    },
+    {
+        id: 2,
+        product_name: "Modern Bathroom Set",
+        description: "Upgraded bathroom products that work as good as you look",
+        price: 250,
+        currency: "$",
+        thumb: "./images/2.png"
+    },
+    {
+        id: 3,
+        product_name: "Classic Maintenance Set",
+        description: "The fundamentals of looking good.",
+        price: 350,
+        currency: "$",
+        thumb: "./images/3.png"
+    },
+    {
+        id: 4,
+        product_name: "Correction Trio Set",
+        description: "Tired of looking tired? There's a set for that.",
+        price: 350,
+        currency: "$",
+        thumb: "./images/4.png"
+    },
+    {
+        id: 5,
+        product_name: "Expert Maintenance Set",
+        description: "The big leagues of skincare.",
+        price: 250,
+        currency: "$",
+        thumb: "./images/5.png"
+    },
+    {
+        id: 6,
+        product_name: "Up Top Management Set",
+        description: "Essential skincare meets essential haircare.",
+        price: 350,
+        currency: "$",
+        thumb: "./images/6.png"
+    },
+    {
+        id: 7,
+        product_name: "Anti-Fatigue Essentials Set",
+        description: "Stop looking tired, start looking refreshed.",
+        price: 350,
+        currency: "$",
+        thumb: "./images/7.png"
+    },
+    {
+        id: 8,
+        product_name: "Youth Management Set",
+        description: "Maintain that always-refreshed look.",
+        price: 250,
+        currency: "$",
+        thumb: "./images/8.png"
+    },
+    {
+        id: 9,
+        product_name: "Fundamental Duo",
+        description: "Your new daily dynamic duo.",
+        price: 350,
+        currency: "$",
+        thumb: "./images/9.png"
+    }
+]
+export default product_card;
